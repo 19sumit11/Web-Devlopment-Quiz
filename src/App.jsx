3906cc08ff8f25ca5988472f193fceba1a979e0c
Sumit shriwastava <sumit87478@gmail.com>
2024-05-12 16:56:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Quiz from './Component/Quiz/Quiz'
+
+function App() {
+  
+
+  return (
+    <>
+      <Quiz/>
+     
+    </>
+  )
+}
+
+export default App
